8e61de0eb5a231b71d46637e88cb7adfa62d5504
Dmytro Maretskyi <maretskii@gmail.com>
2020-06-10 11:06:44 +0200

    Refactor canvas pad

---
+++ pads/canvas-pad/src/shims.d.ts
@@ -0,0 +1,13 @@
+declare module '@dxos/gem-canvas';
+declare module '@dxos/react-client' {
+    type Instance<T> = T extends { new (...args: any): infer U } ? U : never
+
+    declare function useModel<T>(opts: {
+      model: T,
+      options: {
+        type?: string | string[]
+        topic?: string
+        [key: string]: any
+      }
+    }): Instance<T> | null
+}
