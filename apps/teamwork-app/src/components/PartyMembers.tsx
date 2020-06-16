312fa1f40334728d1f54e5d19fd1efb4a611c131
rzadp <przemek@ethworks.io>
2020-06-16 12:12:09 +0200

    Introduced party members

---
+++ apps/teamwork-app/src/components/PartyMembers.tsx
@@ -0,0 +1,44 @@
+//
+// Copyright 2020 DxOS, Inc.
+//
+
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/styles';
+
+import { MemberList } from '@dxos/react-appkit';
+
+const useStyles = makeStyles({
+  memberList: {
+    flex: 1
+  },
+  card: {
+    display: 'flex',
+    flexDirection: 'column',
+    height: 300,
+    width: 250
+  }
+});
+
+export interface PartyMembersProps {
+  party: any,
+}
+
+export const PartyMembers = ({ party }: PartyMembersProps) => {
+  const classes = useStyles();
+
+  const handleCreate = () => {
+
+  };
+
+  return (
+    <Card className={classes.card}>
+      <div className={classes.memberList}><MemberList party={party} /></div>
+      <CardActions>
+        <Button size="small" color="primary" onClick={handleCreate}>Invite member</Button>
+      </CardActions>
+    </Card>
+  );
+};
