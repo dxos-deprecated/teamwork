8b0511232bdc7171527f7a6fd7bb7c44eb98f392
rzadp <przemek@ethworks.io>
2020-06-25 14:56:03 +0200

    Display members in sidebar

--- apps/teamwork-app/src/containers/Sidebar.js
+++ apps/teamwork-app/src/containers/Sidebar.js
@@ -6,16 +6,29 @@ import { Chance } from 'chance';
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import TreeView from '@material-ui/lab/TreeView';
+import { Divider } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
-import { PartyTreeAddItemButton, PartyTreeItem, useAppRouter, usePads } from '@dxos/react-appkit';
+import { PartyTreeAddItemButton, PartyTreeItem, useAppRouter, usePads, MemberList } from '@dxos/react-appkit';
+import { useParty } from '@dxos/react-client';
 
 import { useItems } from '../model';
 import { DocumentTypeSelectDialog } from './DocumentTypeSelectDialog';
 
 const chance = new Chance();
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'grid',
+    gridTemplateRows: '1fr auto',
+    flex: 1
+  }
+}));
+
 export const Sidebar = () => {
   const router = useAppRouter();
+  const party = useParty();
+  const classes = useStyles();
   const { topic, item: active } = useParams();
   const [pads] = usePads();
   const model = useItems(topic);
@@ -34,22 +47,26 @@ export const Sidebar = () => {
   };
 
   return (
-    <TreeView>
-      {model.getAllViews().map(view => (
-        <PartyTreeItem
-          key={view.viewId}
-          id={view.viewId}
-          label={view.displayName}
-          icon={pads.find(pad => pad.type === view.type)?.icon}
-          isSelected={active === view.viewId}
-          onSelect={() => handleSelect(view.viewId)}
-          onUpdate={(title) => model.renameView(view.viewId, title)}
-        />
-      ))}
+    <div className={classes.root}>
+      <TreeView>
+        {model.getAllViews().map(view => (
+          <PartyTreeItem
+            key={view.viewId}
+            id={view.viewId}
+            label={view.displayName}
+            icon={pads.find(pad => pad.type === view.type)?.icon}
+            isSelected={active === view.viewId}
+            onSelect={() => handleSelect(view.viewId)}
+            onUpdate={(title) => model.renameView(view.viewId, title)}
+          />
+        ))}
 
-      <PartyTreeAddItemButton topic={topic} onClick={() => setTypeSelectDialogOpen(true)}>Item</PartyTreeAddItemButton>
+        <PartyTreeAddItemButton topic={topic} onClick={() => setTypeSelectDialogOpen(true)}>Item</PartyTreeAddItemButton>
 
+      </TreeView>
       <DocumentTypeSelectDialog open={typeSelectDialogOpen} onSelect={handleCreate} />
-    </TreeView>
+      <Divider />
+      <MemberList party={party} />
+    </div>
   );
 };
