846cd27d0e4ac224b4898501523ba79c2ccdd930
Dmytro Maretskyi <35851437+Marik-D@users.noreply.github.com>
2020-06-26 15:41:09 +0200

    Add testing pad (#102)
    * Add testing pad
    
    * Lint

---
+++ pads/testing-pad/src/model.js
@@ -0,0 +1,7 @@
+//
+// Copyright 2020 DXOS.org
+//
+
+// TODO(burdon): Define types.
+export const TYPE_TESTING_PAD = 'testing.testing.Pad';
+export const TYPE_TESTING_ITEM = 'testing.testing.Item';
