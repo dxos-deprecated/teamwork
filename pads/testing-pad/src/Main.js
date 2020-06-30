f77099545e1338beecbd58d71d34592a0740cba1
Dmytro Maretskyi <35851437+Marik-D@users.noreply.github.com>
2020-06-30 13:26:03 +0200

    Testing pad improvements (#108)
    * Add stats to testing pad
    
    * Add object property viewer
    
    * Lint

--- pads/testing-pad/src/Main.js
+++ pads/testing-pad/src/Main.js
@@ -2,10 +2,11 @@
 // Copyright 2020 DXOS.org
 //
 
-import React, { useRef } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import ColorHash from 'color-hash';
 import { EchoModel } from '@dxos/echo-db';
 import { useModel, useProfile } from '@dxos/react-client';
+import { JsonTreeView } from '@dxos/react-ux';
 import { TYPE_TESTING_ITEM } from './model';
 
 const colorHash = new ColorHash({ saturation: 1 });
@@ -25,22 +26,35 @@ export const Main = ({ viewId, topic }) => {
     }
   }
 
-  function incrementItem (item) {
-    model.updateItem(item.id, { count: item.properties.count + 1 });
-  }
-
   const renderCount = useRef(0);
   renderCount.current++;
 
+  const [addPeriodically, setAddPeriodically] = useState(false);
+  useEffect(() => {
+    if (addPeriodically) {
+      const intervalId = setInterval(() => {
+        addItem(100);
+      }, 1000);
+      return () => clearInterval(intervalId);
+    }
+  }, [addPeriodically]);
+
+  const [selectedId, setSelectedId] = useState();
+  const selectedItem = selectedId !== undefined ? items.find(i => i.id === selectedId) : undefined;
+
   return (
-    <div>
-      <h1>Render count: {renderCount.current}</h1>
+    <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+      <div>
+        <h4>Render count: {renderCount.current}</h4>
+        <h4>Object count: {items.length}</h4>
+      </div>
       <div>
         <button onClick={() => addItem(1)}>Add 1</button>
         <button onClick={() => addItem(10)}>Add 10</button>
         <button onClick={() => addItem(100)}>Add 100</button>
+        <label>Add 100 every second<input type="checkbox" checked={addPeriodically} onClick={() => setAddPeriodically(x => !x)}></input></label>
       </div>
-      <div>
+      <div style={{ overflowY: 'auto', flex: 1 }}>
         {items.map(item => (
           <div
             key={item.id}
@@ -51,10 +65,17 @@ export const Main = ({ viewId, topic }) => {
               display: 'inline-block',
               backgroundColor: colorHash.hex(item.properties.addedBy?.toString('hex'))
             }}
-            onClick={() => incrementItem(item)}
+            onClick={() => setSelectedId(item.id)}
           >{item.properties.count > 0 ? item.properties.count : ''}</div>
         ))}
       </div>
+      {selectedItem && (
+        <JsonTreeView
+          size="small"
+          depth={2}
+          data={selectedItem}
+        />
+      )}
     </div>
   );
 };
