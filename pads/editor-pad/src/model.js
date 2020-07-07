a7bc7a8f2dde40e8ecf8babac70addebd6cfcd64
rzadp <przemek@ethworks.io>
2020-07-07 13:00:55 +0200

    Add a comment

--- pads/editor-pad/src/model.js
+++ pads/editor-pad/src/model.js
@@ -11,7 +11,7 @@ import { usePads } from '@dxos/react-appkit';
 import { TextModel } from '@dxos/text-model';
 
 export const TYPE_EDITOR_DOCUMENT = 'dxos.teamwork.editor.document';
-export const TYPE_EDITOR_UPDATE = 'dxos.teamwork.editor.update';
+export const TYPE_EDITOR_UPDATE = 'testing.document.Update'; // TODO(rzadp) import TYPE_TEXT_MODEL_UPDATE from text-model after xx gets merged
 
 const chance = new Chance();
 
