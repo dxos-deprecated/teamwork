31de0349dc2f520433b13e672188128dbe44edcf
rzadp <przemek@ethworks.io>
2020-07-07 13:34:45 +0200

    Factor out item creation in Testing

--- pads/testing-pad/src/model.js
+++ pads/testing-pad/src/model.js
@@ -2,5 +2,28 @@
 // Copyright 2020 DXOS.org
 //
 
+import assert from 'assert';
+
+import { ObjectModel } from '@dxos/echo-db';
+import { useModel } from '@dxos/react-client';
+
 export const TYPE_TESTING_PAD = 'dxos.teamwork.testing.Pad';
 export const TYPE_TESTING_ITEM = 'dxos.teamwork.testing.Item';
+
+/**
+ * Provides item list and item creator.
+ * @returns {ViewModel}
+ */
+export const useItems = (topic, viewId) => {
+  const model = useModel({ model: ObjectModel, options: { type: TYPE_TESTING_ITEM, topic, viewId } });
+  const objects = model?.getObjectsByType(TYPE_TESTING_ITEM) ?? [];
+
+  return {
+    model,
+    objects,
+    createItem: (item) => {
+      assert(model);
+      model.createItem(TYPE_TESTING_ITEM, item);
+    }
+  };
+};
