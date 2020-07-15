b6a6dbd8b895f0c39f6ef355d38a32cd91ee98b8
rzadp <przemek@ethworks.io>
2020-07-15 11:34:53 +0200

    Fix for images import

--- apps/teamwork-app/webpack.config.js
+++ apps/teamwork-app/webpack.config.js
@@ -152,7 +152,6 @@ module.exports = {
             loader: 'file-loader',
             options: {
               name: '[name].[ext]',
-              outputPath: 'fonts/'
             }
           }
         ]
