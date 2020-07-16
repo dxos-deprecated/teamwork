1bd507183a3f86c8fea27cdd64e2f920a4de586b
rzadp <przemek@ethworks.io>
2020-07-16 13:25:02 +0200

    Fix for https://github.com/dxos/teamwork/issues/196

--- pads/messenger-pad/src/ephemeral-swarm.js
+++ pads/messenger-pad/src/ephemeral-swarm.js
@@ -26,7 +26,7 @@ export const useEphemeralSwarm = (channelId) => {
   const channelDiscoveryKey = discoveryKey(keyToBuffer(`${channelId}-video`));
 
   useEffect(() => {
-    const { signal, ice } = client.config.swarm;
+    const { signal, ice } = client._swarmConfig;
     const swarm = discoverySwarmWebRTC({
       bootstrap: [signal],
       simplePeer: {
