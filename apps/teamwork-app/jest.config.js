e1535b3acc22e0871cdc00d6230027ac41cff5d2
HubertNastaly <h.nastaly@gmail.com>
2020-10-15 16:34:42 +0200

    Simple Pad render test

--- apps/teamwork-app/jest.config.js
+++ apps/teamwork-app/jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   rootDir: '../..',
   transform: {
-    '^apps/teamwork-app/src/tests/.+(js|jsx)$': '<rootDir>/node_modules/babel-jest',
-    '^node_modules/@dxos/.+(js|jsx)$': '<rootDir>/node_modules/babel-jest'
+    '^.+/src/.+(js|jsx)$': '<rootDir>/node_modules/babel-jest',
+    '^.+/dist/.+(js|jsx)$': '<rootDir>/node_modules/babel-jest'
   },
   transformIgnorePatterns: [
     'node_modules/(?!(@dxos)/)'
--- apps/teamwork-app/src/tests/empty-test.test.js
+++ apps/teamwork-app/src/tests/empty-test.test.js
@@ -1,9 +1,13 @@
 //
 // Copyright 2020 DXOS.org
 //
+import { render } from '@testing-library/react';
+import React from 'react';
+
+import Pad from '../../../../pads/editor-pad/src/components/Pad';
 
 describe('Simple test to check jest coverage action', () => {
-    test('2+2=4', () => {
-        expect(2 + 2).toEqual(4);
+    test('Render pad', async () => {
+        render(<Pad />);
     });
 });
