a6ffbaaee4e722ab450a8850be819b791ef98695
HubertNastaly <h.nastaly@gmail.com>
2020-10-15 16:18:54 +0200

    Lint fix

--- apps/teamwork-app/src/tests/empty-test.test.js
+++ apps/teamwork-app/src/tests/empty-test.test.js
@@ -1,5 +1,9 @@
+//
+// Copyright 2020 DXOS.org
+//
+
 describe('Simple test to check jest coverage action', () => {
     test('2+2=4', () => {
-        expect(2+2).toEqual(4);
-    })
-})
+        expect(2 + 2).toEqual(4);
+    });
+});
