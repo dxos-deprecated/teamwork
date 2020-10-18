f743271f77d51f6dd69cb93c4a2a2d06395c5acb
richburdon <rich.burdon@gmail.com>
2020-10-18 09:59:49 -0600

    Check errors on startup.

--- apps/teamwork-app/src/main.js
+++ apps/teamwork-app/src/main.js
@@ -18,29 +18,51 @@ import Root from './containers/Root';
 (async () => {
   const cfg = await loadConfig();
 
+  // TODO(burdon): Why isn't config passed to the app? Make canonical.
   const {
     client: { feedStorage, keyStorage, swarm },
     services: { wns: { server, chainId }, ipfs },
     ...config
   } = cfg.values;
 
+  debug.enable(cfg.get('debug.logging', 'dxos:*')); // TODO(burdon): Use .? syntax.
+
+  // TODO(burdon): Change API to return class (not function with expando properties).
+  const storage = createStorage(feedStorage.root, feedStorage.type);
   const keyring = new Keyring(new KeyStore(leveljs(`${keyStorage.root}/keystore`)));
+  const registry = new Registry(server, chainId);
 
   const client = new Client({
-    storage: createStorage(feedStorage.root, feedStorage.type),
+    storage,
     keyring,
-    swarm,
-    registry: new Registry(server, chainId)
+    registry,
+    swarm
   });
-  await client.initialize();
 
-  debug.enable(cfg.get('debug.logging'));
+  try {
+    await client.initialize();
 
-  ReactDOM.render(
-    <Root
-      config={{ ipfs, ...config }}
-      client={client}
-    />,
-    document.getElementById(cfg.get('app.rootElement'))
-  );
+    ReactDOM.render(
+      <Root
+        config={{ ipfs, ...config }}
+        client={client}
+      />,
+      document.getElementById(cfg.get('app.rootElement'))
+    );
+  } catch (ex) {
+    // TODO(burdon): Do something different in prod. (Factor out error control with reset button).
+    console.error(ex);
+    console.log(JSON.stringify(cfg.values, undefined, 2));
+    ReactDOM.render(
+      <div>
+        <h1>ERROR</h1>
+        <div>{String(ex)}</div>
+        <br />
+        <button onClick={() => {
+          storage.destroy();
+        }}>Reset storage</button>
+      </div>,
+      document.body
+    );
+  }
 })();
