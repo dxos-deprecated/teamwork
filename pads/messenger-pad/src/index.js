a000cff80e2887f8fae7adf01deb3cb9c9a6b82d
rzadp <roopert7@gmail.com>
2020-10-21 15:07:48 +0200

    feat: Bring back messenger settings (chat export).
    Fix too short data type for timestamps

--- pads/messenger-pad/src/index.js
+++ pads/messenger-pad/src/index.js
@@ -7,7 +7,7 @@ import Icon from '@material-ui/icons/Chat';
 import { MessengerModel } from '@dxos/messenger-model';
 import { ObjectModel } from '@dxos/object-model';
 
-import { Channel } from './containers';
+import { Channel, MessengerSettingsDialog } from './containers';
 import { TYPE_MESSENGER_CHANNEL, TYPE_MESSENGER_MESSAGE } from './model';
 
 export * from './model';
@@ -22,7 +22,7 @@ export default {
   type: TYPE_MESSENGER_CHANNEL,
   contentType: TYPE_MESSENGER_MESSAGE,
   description: 'Chat with friends',
-  // settings: MessengerSettingsDialog
+  settings: MessengerSettingsDialog,
   register: async (client) => {
     await client.modelFactory.registerModel(MessengerModel);
   },
