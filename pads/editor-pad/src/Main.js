f8efc874c62da15482a58c07a7c4930d97b5d3e4
Rich Burdon <rich.burdon@gmail.com>
2020-10-28 15:33:23 -0400

    burdon/margin (#354)
    * Fix margins.
    
    * Fix width.

--- pads/editor-pad/src/Main.js
+++ pads/editor-pad/src/Main.js
@@ -30,7 +30,8 @@ const useStyles = makeStyles(theme => ({
     [theme.breakpoints.up('subMbp')]: {
       margin: 'auto'
     },
-    maxWidth: 800
+    maxWidth: 800,
+    width: 800
   },
 
   messengerContainer: {
