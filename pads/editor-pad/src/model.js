10e6e9da78db9fccda34c9da609f2a8e60776f28
rzadp <roopert7@gmail.com>
2020-10-30 12:43:00 +0100

    Bump ECHO/SDK

--- pads/editor-pad/src/model.js
+++ pads/editor-pad/src/model.js
@@ -6,11 +6,10 @@ import assert from 'assert';
 
 import { keyToBuffer } from '@dxos/crypto';
 import { useItems } from '@dxos/react-client';
-import { TYPE_TEXT_MODEL_UPDATE } from '@dxos/text-model'; // TODO(burdon): Standardize exported consts.
 
 export const EDITOR_PAD = 'dxos.org/pad/editor';
-export const EDITOR_TYPE_DOCUMENT = 'dxos.org/type/editor/document';
-export const EDITOR_TYPE_UPDATE = TYPE_TEXT_MODEL_UPDATE;
+export const EDITOR_TYPE_DOCUMENT = 'dxos.org/type/editor/document@v2';
+export const EDITOR_TYPE_CONTENT = 'dxos.org/type/editor/document/content';
 
 /**
  * Provides the document content.
@@ -18,6 +17,6 @@ export const EDITOR_TYPE_UPDATE = TYPE_TEXT_MODEL_UPDATE;
 export const useDocumentUpdateModel = (topic, documentId) => {
   assert(topic);
   assert(documentId);
-  const [editor] = useItems({ partyKey: keyToBuffer(topic), parent: documentId, type: EDITOR_TYPE_UPDATE });
-  return editor && editor.model && editor.model.model; // Using adapter.
+  const [editor] = useItems({ partyKey: keyToBuffer(topic), parent: documentId, type: EDITOR_TYPE_CONTENT });
+  return editor && editor.model; // Using adapter.
 };
