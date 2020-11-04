71d25bf17a34352a9dc6b3bf09a3e8922a1fb913
HubertNastaly <56291339+HubertNastaly@users.noreply.github.com>
2020-11-04 15:19:56 +0100

    feat: Sort messages by timestamp (#377)
    * Sort messages by timestamp
    
    * Sort on message push

--- models/messenger-model/src/messenger-model.ts
+++ models/messenger-model/src/messenger-model.ts
@@ -21,7 +21,8 @@ export class MessengerModel extends Model<Message> {
   }
 
   async _processMessage (meta: MutationMeta, message: Message) {
-    this._messages.push(message)
+    this._messages.push(message);
+    this._messages.sort((msgA, msgB) => parseInt(msgA.timestamp) - parseInt(msgB.timestamp));
     return true;
   }
 
