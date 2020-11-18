dc5f4beb51cbfd5f4e7af7757c02a61a5ef0176e
HubertNastaly <h.nastaly@gmail.com>
2020-11-18 11:52:45 +0100

    Extract starting script and add strategy matrix

---
+++ scripts/start-and-wait.sh
@@ -0,0 +1,13 @@
+yarn start & 
+set +e
+i=0
+until curl "http://localhost:8080" | grep -q "Teamwork"
+do
+  ((i++))
+  if [ "$i" -gt 30 ]
+  then
+    exit -1
+  fi
+  echo "Sleeping..."
+  sleep 3
+done
