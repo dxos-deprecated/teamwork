1e3ccb437a83080652a8dc5453bfe037cb24aec6
rzadp <roopert7@gmail.com>
2020-12-02 17:46:22 +0100

    WIP: export

--- apps/teamwork-app/src/containers/Home.js
+++ apps/teamwork-app/src/containers/Home.js
@@ -8,13 +8,15 @@ import React, { useState } from 'react';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/styles';
 
+import { schema } from '@dxos/echo-protocol';
 import {
   AppContainer,
   PartyCard,
   PartyCardContainer,
   IpfsHelper,
   PartyFromFileDialog,
-  PartyFromIpfsDialog
+  PartyFromIpfsDialog,
+  usePads
 } from '@dxos/react-appkit';
 import { useClient, useParties, useConfig } from '@dxos/react-client';
 
@@ -49,6 +51,7 @@ const Home = () => {
   const client = useClient();
   const parties = useParties();
   const config = useConfig();
+  const [pads] = usePads();
 
   const [inProgress, setInProgress] = useState(false);
   const [partyFromFileOpen, setPartyFromFileOpen] = useState(false);
@@ -87,12 +90,20 @@ const Home = () => {
   };
 
   const handleImport = async (data) => {
-    const parsed = JSON.parse(data);
-    assert(Array.isArray(parsed));
-    const newParty = await client.echo.createParty();
-    const newPartyTopic = newParty.key.toHex();
-    const newPartyModel = await client.modelFactory.createModel(undefined, { type: [], topic: newPartyTopic });
-    parsed.forEach(msg => newPartyModel.appendMessage(msg));
+    console.log('handleimport', data);
+    const bufferData = Buffer.from(data, 'hex')
+    console.log('bufferData', bufferData)
+    const decodedSnapshot = schema.getCodecForType('dxos.echo.snapshot.DatabaseSnapshot').decode(bufferData);
+    console.log('decodedSnapshot', decodedSnapshot)
+
+    await client.createPartyFromSnapshot(decodedSnapshot)
+
+    // const parsed = JSON.parse(data);
+    // assert(Array.isArray(parsed));
+    // const newParty = await client.echo.createParty();
+    // const newPartyTopic = newParty.key.toHex();
+    // const newPartyModel = await client.modelFactory.createModel(undefined, { type: [], topic: newPartyTopic });
+    // parsed.forEach(msg => newPartyModel.appendMessage(msg));
   };
 
   return (
@@ -129,3 +140,25 @@ const Home = () => {
 };
 
 export default Home;
+
+// TODO: use from echo-db
+export function sortItemsTopologically (items) {
+  const snapshots = [];
+  const seenIds = new Set();
+
+  while (snapshots.length !== items.length) {
+    const prevLength = snapshots.length;
+    for (const item of items) {
+      assert(item.itemId);
+      if (!seenIds.has(item.itemId) && (item.parentId == null || seenIds.has(item.parentId))) {
+        snapshots.push(item);
+        seenIds.add(item.itemId);
+      }
+    }
+    if (prevLength === snapshots.length && snapshots.length !== items.length) {
+      throw new Error('Cannot topologically sorts items in snapshot: some parents are missing.');
+    }
+  }
+
+  return snapshots;
+}
