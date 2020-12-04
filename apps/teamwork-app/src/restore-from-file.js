1391b5aa904fac1daf577423259f1a8f27601990
rzadp <roopert7@gmail.com>
2020-12-04 14:51:39 +0100

    Finished tests

--- apps/teamwork-app/src/restore-from-file.js
+++ apps/teamwork-app/src/restore-from-file.js
@@ -5,10 +5,7 @@
 import { schema } from '@dxos/echo-protocol';
 
 export const restoreFromFile = async (client, data) => {
-  const bufferData = Buffer.from(data, 'hex')
+  const bufferData = Buffer.from(data, 'hex');
   const decodedSnapshot = schema.getCodecForType('dxos.echo.snapshot.DatabaseSnapshot').decode(bufferData);
-  const restoredParty = await client.createPartyFromSnapshot(decodedSnapshot)
-
-  // console.log('restoredParty', restoredParty)
-  return restoredParty
-}
+  return client.createPartyFromSnapshot(decodedSnapshot);
+};
