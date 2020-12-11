6d51d7b6278921669b825e4e9866e6e83a091ec7
HubertNastaly <h.nastaly@gmail.com>
2020-12-11 09:53:02 +0100

    Wait for task

--- apps/teamwork-app/tests/playwright/utils/TasksManager.js
+++ apps/teamwork-app/tests/playwright/utils/TasksManager.js
@@ -33,6 +33,7 @@ export class TasksManager {
 
   async deleteTask (taskName) {
     const deleteButtonSelector = `//li[.//*[text()="${taskName}"]]//button`;
+    await this.isTaskExisting(taskName);
     await this.page.click(deleteButtonSelector);
   }
 
