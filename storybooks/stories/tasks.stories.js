78ebbafb76ad3a37b536cd0eea61e78e3ef82908
rzadp <roopert7@gmail.com>
2021-01-11 12:35:57 +0100

    Add tasks story

---
+++ storybooks/stories/tasks.stories.js
@@ -0,0 +1,21 @@
+//
+// Copyright 2020 DXOS.org
+//
+
+import React from 'react';
+
+import pad from '@dxos/tasks-pad';
+import StorybookInitializer from './StorybookInitializer';
+
+export default {
+  title: 'Tasks pad'
+};
+
+export const withTasksPad = () => {
+  return (
+    <StorybookInitializer
+      createItem={pad.create}
+      pad={pad.main}
+    />
+  );
+};
