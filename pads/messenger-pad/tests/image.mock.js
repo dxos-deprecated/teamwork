6a4343d76c31387247cff173edb82c91cf06c580
Przemyslaw Rzad <przemek@ethworks.io>
2021-01-21 12:39:10 +0200

    chore: TSC in pads (#547)
    * TSC in pads
    
    * Rename to jsx
    
    * Sort out version.json import
    
    * Marry jest and ts
    
    * Fix for images
    
    * Remove babelrc
    
    * Mock images
    
    * Sort out jest
    
    * CI trigger
    
    * Attempt to speed up jest tests

---
+++ pads/messenger-pad/tests/image.mock.js
@@ -0,0 +1,6 @@
+//
+// Copyright 2020 DXOS.org
+//
+
+// Needed for jest tests
+export default '';
--- pads/table-pad/jest.config.js
+++ pads/table-pad/jest.config.js
@@ -3,11 +3,20 @@
 //
 
 module.exports = {
+  testEnvironment: 'jsdom',
+  preset: 'ts-jest/presets/js-with-ts',
   coverageDirectory: 'coverage',
   collectCoverageFrom: [
     'src/**/*.{js,jsx}'
   ],
-  transformIgnorePatterns: [
-    'node_modules/(?!(@dxos)/)'
-  ]
+  // transformIgnorePatterns: [
+  //   'node_modules/(?!(@dxos)/)'
+  // ],
+
+  // https://stackoverflow.com/a/60905543
+  globals: {
+    'ts-jest': {
+      isolatedModules: true
+    }
+  }
 };
